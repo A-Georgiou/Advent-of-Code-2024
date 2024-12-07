87928dbdcc4bb2a3981b5856699369d765fbc2ec
Andrew Georgiou <andrewgeorgiou98@outlook.com>
2024-12-07 16:05:33 +0000

    Advent of Code - Day 07
    Quick and simple, thank you Advent of Code team for letting me enjoy the football this weekend.
    
    Part 1 - We built a quick BFS since we are only constructing a binary tree. I considered optimisations like checking if we exceed the resultant value to exit early but the runtime is quick enough to not worry.
    
    Part 2 - Thankfully our first solution enabled us to convert this binary tree to a tertiary tree with little performance drop.

---
+++ day-07/part2.py
@@ -0,0 +1,30 @@
+import os
+import sys
+from collections import deque
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'utils'))
+from parser import Parser
+
+def bfs(result, numbers):
+    queue = deque([(numbers[0], 0)])
+
+    while queue:
+        current, idx = queue.popleft()
+
+        if idx == len(numbers) - 1:
+            if current == result:
+                return True
+            continue
+
+        next_num = numbers[idx + 1]
+        queue.append((current + next_num, idx + 1))
+        queue.append((current * next_num, idx + 1))
+        queue.append((int(str(current) + str(next_num)), idx + 1))
+    return False
+
+def find_result():
+    math_operations = Parser(file_path='input.txt').parse_lines(str, delimiter=': ')
+    math_operations = [[int(x), [int(k) for k in y.split()]] for x, y in math_operations]
+    return sum(result if bfs(result, numbers) else 0 for result, numbers in math_operations)
+
+if __name__ == "__main__":
+    print("Combined Result:", find_result())
